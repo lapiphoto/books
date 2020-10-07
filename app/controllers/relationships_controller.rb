fd0e8a1ce7c9959c083ff75fafcc3f83f034e37f
vagrant <vagrant@localhost.localdomain>
2020-10-07 18:10:35 +0000

    Initial Commit

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,34 @@
+class RelationshipsController < ApplicationController
+	before_action :set_user, except: [:index, :show]
+
+	def index
+		@user = User.find(params[:id])
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def create
+		following = current_user.follow(@user)
+		if following.save
+			redirect_to request.referer
+		else
+			redirect_to request.referer
+		end
+	end
+
+	def destroy
+		following = current_user.unfollow(@user)
+		if following.destroy
+			redirect_to request.referer
+		else
+			redirect_to request.referer
+		end
+	end
+
+	private
+	def set_user
+		@user = User.find(params[:follow_id])
+	end
+end
